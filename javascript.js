9382d5f68abb860fddd003e088998febae7b6a75
Sayma <94951703+sayma1976@users.noreply.github.com>
2021-12-18 12:50:16 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+let hallo = 'Hello Winc Academy';
+console.log(hallo);
+let firstName = 'Sayma';
+console.log(firstName);
+const a = 4;
+console.log(a + a);
+console.log(a * 2);
+console.log(a / 2);
+console.log(a - 1);
+console.log(a % 2);
+console.log(-15 % 7);
+console.log(a);
+let age = 45;
+console.log(typeof age);
+age = '45';
+console.log(typeof age);
