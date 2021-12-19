1714c66a76a6564b42a2663d822b0df0aea81db1
Sayma <94951703+sayma1976@users.noreply.github.com>
2021-12-19 15:12:51 +0100

    Add files via upload

---
+++ opdracht.js
@@ -0,0 +1,21 @@
+const age = 20;
+if (age >= 18) {
+    console.log('jij mag naar binnen');
+} else if (age < 18) {
+    console.log('wil je buiten blijven, jij mag nog niet naar binnen');
+}
+const isFemale = false;
+if (isFemale) {
+    console.log('Let\'s party!');
+
+} else {
+    console.log('Sorry, vandaag is ladies night');
+
+}
+const driverStatus = 'bob';
+if (driverStatus === 'bob') {
+    console.log("jij mag rijden")
+
+} else {
+    console.log('jij mag niet rijden')
+}
