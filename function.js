f25a8e58d12ff7a96b42fb69aedf979572c95388
Sayma <94951703+sayma1976@users.noreply.github.com>
2021-12-21 16:36:09 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,26 @@
+function makePizza() {
+    console.log('Maken pizza.');
+    console.log('Deeg maken van bloem, zout, olie, gist, water.');
+    console.log('Rol deeg op de tafel plat rond form.');
+    console.log('Smeer deeg met tomatensaus.');
+    console.log('Leg op de pizza jouw ingridienten.');
+    console.log('Strooi geraspte kaas over jouw pizza.');
+    console.log('Bak pizza in de oven temperatuur 220 graden 12 minuten.')
+
+}
+//makePizza();
+//makePizza();
+//makePizza();
+function makeSushi() {
+    console.log('Maken sushi');
+    console.log('Kook rijst.')
+    console.log('bereid de ingridienten voor sushi-vis, zeewier, wasabi, sojasaus.');
+    console.log('Leg gekoelde rijst op de zeewier ongeveer 0.5cm dik.');
+    console.log('Leg streep van de vis filee en rol rijst met zeewier in mooie rol.');
+    console.log('Snijd sushi rollen, serveer met wasabi en sojasaus.');
+}
+//makeSushi();
+let order = 'sushi x 3. pizza x 2'
+console.log(order);
+makeSushi(); makeSushi(); makeSushi();
+makePizza(); makePizza();
